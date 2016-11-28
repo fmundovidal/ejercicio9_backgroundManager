7401b867b6e75820f50efbc19e6663260fd489d8
ferran <fmundovidal@gmail.com>
2016-11-28 16:03:16 +0100

    works

--- app/src/main/java/com/example/a5alumno/ejercicio9_backgroundmanager/MainActivity.java
+++ app/src/main/java/com/example/a5alumno/ejercicio9_backgroundmanager/MainActivity.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG_MAIN_ACTIVITY = MainActivity.class.getSimpleName();
+    private ProgressBar mProgressbar;
+    private static final String FINISH_THREAD= "Thread finished";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +24,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btnStartThread = (Button)this.findViewById(R.id.btn_start_thread);
         btnStartThread.setOnClickListener(this);
         Button btnStartAsyncTask = (Button)this.findViewById(R.id.btn_start_async_task);
-        btnStartThread.setOnClickListener(this);
+        btnStartAsyncTask.setOnClickListener(this);
+        this.mProgressbar = (ProgressBar)this.findViewById(R.id.progressBar);
+        this.mProgressbar.setProgress(0);
 
     }
 
     @Override
     public void onClick(View view) {
+        mProgressbar.setProgress(0);
+        if(view.getId()==R.id.btn_start_async_task){
+            new MyAsyncTask(this).execute(1);
+        }
+        else if(view.getId()==R.id.btn_start_thread){
+            new Thread(new Runnable(){
+
+                @Override
+                public void run() {
+                    Log.i(MainActivity.TAG_MAIN_ACTIVITY,"Thread started");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Sleeping...", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    sleepForAWhile(10);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext() , FINISH_THREAD, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                   //
+                }
+
+            }).start();
+        }
 
     }
 
     public class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
         private Context mContext;
+
+        public MyAsyncTask(Context anyContext){
+            this.mContext = anyContext;
+        }
         @Override
         protected String doInBackground(Integer... params) {
             for(int idx=1;idx<=5;idx++){
                 sleepForAWhile(params[0]);
+                publishProgress(idx*20);
             }
             return "AsyncTask finished";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            //((MainActivity)mContext).mProgressbar.setProgress(0);
+            mProgressbar.setProgress(values[0]);
         }
 
         @Override
